9f653127413e23d920b67bd243f4fe2aa190e708
Nino <valleysofpluto@gmail.com>
2026-01-11 17:30:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9381016045-4d625",
+  "appId": "1:733431756980:web:70c581646d298c84ed7fe1",
+  "apiKey": "AIzaSyAQtEF3XZKZiAXcmqhofEXdSRB1IZTzMdo",
+  "authDomain": "studio-9381016045-4d625.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "733431756980"
+};
